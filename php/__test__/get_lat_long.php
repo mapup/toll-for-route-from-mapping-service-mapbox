d09f67a8df83979cbab27dc5d580ac6f74df968f
Asijit Paul <asijit1610@gmail.com>
2021-01-19 15:13:17 +0530

    test function added

---
+++ php/__test__/get_lat_long.php
@@ -0,0 +1,27 @@
+<?php
+function getCord($address){
+$key = 'mapbox_api_key';
+
+$url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'.urlencode($address).'.json?access_token='.$key.'';
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$responseJson = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($responseJson, true);
+
+$location = array(
+	'x' => $response['features']['0']['center']['1'],
+    'y' => $response['features']['0']['center']['0']
+);
+
+return $location;
+ }
+?>
